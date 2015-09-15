76ddafcadb8b457934a50ebfee7e5ee2d4b43191
Tim Dorr <timdorr@timdorr.com>
2015-09-15 13:05:54 -0400

    Fix some linting errors...

--- examples/real-world/createDevToolsWindow.js
+++ examples/real-world/createDevToolsWindow.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
-/**
+/*
  * Puts Redux DevTools into a separate window.
  * Based on https://gist.github.com/tlrobinson/1e63d15d3e5f33410ef7#gistcomment-1560218.
  */
 export default function createDevToolsWindow(store) {
   // Window name.
   const name = 'Redux DevTools';
-  
+
   // Give it a name so it reuses the same window.
   const win = window.open(
     null,
@@ -19,7 +19,7 @@ export default function createDevToolsWindow(store) {
 
   // Reload in case it's reusing the same window with the old content.
   win.location.reload();
-  
+
   // Set visible Window title.
   win.document.title = name;
 
